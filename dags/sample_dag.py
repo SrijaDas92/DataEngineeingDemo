764a649a26d7f6ef5f2b215611529e703fa44f2a
Srija Das <100459730+SrijaDas92@users.noreply.github.com>
2025-06-17 15:07:08 +0530

    Update sample_dag.py

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -2,16 +2,15 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-def say_hello():
-    print("Hello, world!")
+
+def hello_world():
+    print("Hello, Airflow!")
+
 
 with DAG(
-    dag_id="hello_dag",
+    "hello_world_dag",
     start_date=datetime(2024, 1, 1),
     schedule_interval="@daily",
     catchup=False,
 ) as dag:
-    hello_task = PythonOperator(
-        task_id="say_hello",
-        python_callable=say_hello
-    )
+    task = PythonOperator(task_id="say_hello", python_callable=hello_world)
